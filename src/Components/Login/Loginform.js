959cb34700994bafbddba7815358570f645caef6
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-12-05 03:04:19 +0530

    up

--- src/Components/Login/Loginform.js
+++ src/Components/Login/Loginform.js
@@ -1,9 +1,9 @@
 import React from "react";
-import './Loginform.css';
+import "./Loginform.css";
 import Logo from "../../Assets/Logo/rojgar.png";
-import Banner from "../../Assets/Images/workerimage.jpg";
 import LoginInput from "./LoginInput";
-import GmailLogin from './GmailLogin'
+import { FiArrowRight} from "react-icons/fi";
+import GmailLogin from "./GmailLogin";
 export default function Loginform() {
   return (
     <div className="login_cover">
@@ -13,16 +13,30 @@ export default function Loginform() {
         </div>
         <div className="login_heading">Log in to your account</div>
         <GmailLogin />
-        <div className="login_or"><span>or</span></div>
+        <div className="login_or">
+          <span>or</span>
+        </div>
         <LoginInput name="Email Address" />
         <LoginInput name="Password" />
         <div className="login_submit">
-            <button>Login</button>
-            <span>Don't have an account?<span className="login_change">&nbsp;Sign Up</span></span>
+          <button>Login</button>
+          <span>
+            Don't have an account?
+            <span className="login_change">&nbsp;Sign Up</span>
+          </span>
         </div>
       </div>
       <div className="login_section2">
-          <img src={Banner} alt="banner image"></img>
+        <div className="tag_1">
+          Get Your Job Done <br></br> By The Professionals.
+        </div>
+        <div className="tag_content_1">
+          Book Online workers for the several <br></br>job types who are honest and hard<br></br>working at your finger tips. 
+        </div>
+        <span className="join_now1">
+          Become a Professional &nbsp;
+        </span>
+        {/* <FiArrowRight /> */}
       </div>
     </div>
   );
